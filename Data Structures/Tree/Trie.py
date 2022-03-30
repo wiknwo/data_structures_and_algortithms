e9dc1dcd71387ae27f67f28df222bb839346ec91
wiknwo <spinningthewww@outlook.com>
2022-03-30 21:06:31 +0100

    Added functionality to search, get string representation of and delete from trie.

--- Data Structures/Tree/Trie.py
+++ Data Structures/Tree/Trie.py
@@ -20,25 +20,26 @@ class TrieNode:
         self.children = {}
 
 class Trie:
-    """Class representing a Trie
+    """
+    Class representing a Trie
     
-        Trie is a very useful data structure. It is 
-        commonly used to represent a dictionary for 
-        looking up words in a vocabulary. 
+    Trie is a very useful data structure. It is 
+    commonly used to represent a dictionary for 
+    looking up words in a vocabulary. 
 
-        For example, consider the task of implementing a 
-        search bar with auto-completion or query 
-        suggestion. When the user enters a query, the 
-        search bar will automatically suggests common 
-        queries starting with the characters input by 
-        the user.
+    For example, consider the task of implementing a 
+    search bar with auto-completion or query 
+    suggestion. When the user enters a query, the 
+    search bar will automatically suggests common 
+    queries starting with the characters input by 
+    the user.
 
-        Trie is a tree-like data structure made up of nodes. 
-        Nodes can be used to store data. Each node may have 
-        none, one or more children. When used to store a 
-        vocabulary, each node is used to store a character, 
-        and consequently each "branch" of the trie represents 
-        a unique word. 
+    Trie is a tree-like data structure made up of nodes. 
+    Nodes can be used to store data. Each node may have 
+    none, one or more children. When used to store a 
+    vocabulary, each node is used to store a character, 
+    and consequently each "branch" of the trie represents 
+    a unique word. 
 
     Attributes:
         root: The root of the trie
@@ -78,7 +79,8 @@ class Trie:
         node.counter += 1
         
     def dfs(self, node, prefix):
-        """Depth-first traversal of the trie
+        """
+        Depth-first traversal of the trie
         
         Args:
             - node: the node to start with
@@ -92,8 +94,19 @@ class Trie:
         for child in node.children.values():
             self.dfs(child, prefix + node.char)
         
+    def search(self, word):
+        """Method to return boolean indicating if word exists in trie"""
+        node = self.root
+        for char in word:
+            if char in node.children:
+                node = node.children[char]
+            else:
+                return False
+        return node.is_end
+
     def query(self, x):
-        """Given an input (a prefix), retrieve all words stored in
+        """
+        Given an input (a prefix), retrieve all words stored in
         the trie with that prefix, sort the words by the number of 
         times they have been inserted
         """
@@ -115,11 +128,73 @@ class Trie:
 
         # Sort the results in reverse order and return
         return sorted(self.output, key=lambda x: x[1], reverse=True)
-        
+
+    def __str__(self):
+        """Method to return string representation of all words in trie"""
+        # Traverse the trie to get all candidates
+        self.output = []
+        self.dfs(self.root, "")
+        return '\n'.join(pair[0] for pair in sorted(self.output, key = lambda pair: pair[0]))
+
+    def delete(self, word):
+        """
+        Method to delete word from trie if it exists
+
+        1. Find word if it exists
+        2. If the node exists and it has children then 
+        we can't delete it and set the is_word flag to False
+        3. If the node has no children then we can delete it
+        and check if we can delete the parent
+        4. If the node has no children and its counter is 0
+        then delete it and check if we can delete its parent
+        """
+        node, parent = self.root, None
+
+        # Find word in trie if it exists
+        for char in word:
+            if char in node.children:
+                parent = node
+                node = parent.children[char]
+            else:
+                # If character not in trie then word 
+                # does not exist and we return False 
+                # as we did not delete anything
+                return False
+        # Decrement the counter for this word
+        print("Word on this recurse: {}, {}".format(word, node.children))
+        if node.is_end:
+            node.counter -= 1
+            if not node.children and node.counter == 0:
+                print("No children and counter == 0: {}".format(word))
+                node.is_end = False
+                node = None
+                del parent.children[word[-1]]
+                self.delete(word[:-1])
+        elif not node.children and not node.is_end:
+            print("Node has no children and it is not a word")
+            del parent.children[word[-1]]
+            node = None
+            self.delete(word[:-1])
+
+            
 t = Trie()
 t.insert('was')
 t.insert('word')
 t.insert('war')
 t.insert('what')
 t.insert('where')
+t.insert('where')
 print(t.query('wh'))
+print(t)
+print(t.search('where'))
+t.delete('where')
+t.delete('where')
+print(t.query('wh'))
+print(t)
+t.delete('what')
+print(t)
+t.insert('what')
+t.insert('where')
+t.insert('what')
+print(t.query('wh'))
+print(t)
